a66bf3423c4431cb495a621550bb962096657aae
mateusfprog <mateus102006@hotmail.com>
2024-05-13 09:42:09 -0300

    tipos primitivos

---
+++ c-tiposprimitivos.js
@@ -0,0 +1,20 @@
+// Tipos primitivos em JavaScript
+
+let nome = "Mateus";          // String literal
+let idade = 26;               // Number literal
+let altura = 1.80             // Number literal
+let estaAprovado = true       // boolean
+let sobrenome;                // Undefined (nao definida)
+let corSelecionada = null     // Object
+
+// number
+console.log(typeof(idade))
+
+// string
+console.log(typeof(nome))
+
+//undefined
+console.log(typeof(sobrenome))
+
+// Object
+console.log(typeof(corSelecionada))
