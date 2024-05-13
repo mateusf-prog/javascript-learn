b277d352c47f7f73c5f065638b348a7cd241faf4
mateusfprog <mateus102006@hotmail.com>
2024-05-13 09:42:00 -0300

    variaveis e constantes

---
+++ a-variaveis.js
@@ -0,0 +1,11 @@
+// tipagem dinamica 
+// case sensitive
+// camelCase
+
+let idade = 5
+let altura = 1.80
+let nome = "Mateus"
+let nomeSobrenome = "Mateus Fonseca"
+
+// Meu nome e sobrenome é Mateus Fonseca e minha altura é 1.8
+console.log("Meu nomee sobrenome é " + nomeSobrenome + " e minha altura é " + altura)
---
+++ b-constantes.js
@@ -0,0 +1,6 @@
+// pode ser alterado em qualquer momento no codigo
+let valorIngresso = 20
+valorIngresso = 30
+
+// uma constante nao pode ser alterada após seu valor definido
+const valorIngressoAdulto = 30
