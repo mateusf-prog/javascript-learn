ab7d696d82f885385f4a5407908fbcace2cfd870
mateusfprog <mateus102006@hotmail.com>
2024-05-13 09:49:51 -0300

    exercicio-variaveis

--- a-variaveis.js
+++ a-variaveis.js
@@ -9,3 +9,10 @@ let nomeSobrenome = "Mateus Fonseca"
 
 // Meu nome e sobrenome é Mateus Fonseca e minha altura é 1.8
 console.log("Meu nomee sobrenome é " + nomeSobrenome + " e minha altura é " + altura)
+
+// Exercicio trocando os valores das variaveis
+let a = 'vermelho'
+let b = 'azul'
+let c = a
+a = b
+b = c
