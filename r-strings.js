5ab3cf0936b81176abdaada31484edc320f33e74
mateusfprog <mateus102006@hotmail.com>
2024-05-19 09:41:07 -0300

    String - Objeto e Tipo primitivo

---
+++ r-strings.js
@@ -0,0 +1,13 @@
+// Estudo de Strings
+
+// Tipos primitivos
+const mensagem = "Olá, Mundo!"
+
+
+// Tipo objeto (contém funcionalidades da classe String)
+const mensagem2 = new String("Olá, Mundo")
+
+console.log(mensagem2.length)  // tamanho da string
+console.log(mensagem2[2])      // índice 2 da string
+console.log(mensagem2.includes("Olá")) // verfica se existe o "Olá" dentro da string
+console.log(mensagem2.startsWith("Olá")) // // verfica se string inicia com "Olá"
